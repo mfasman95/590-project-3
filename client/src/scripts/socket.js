56d7347d95744d3b2bab431a1c1bcf56b6997aae
Matthew Fasman <mfasman95@gmail.com>
2017-11-15 22:33:01 -0500

    Testing Heroku Bug

--- client/src/scripts/socket.js
+++ client/src/scripts/socket.js
@@ -38,6 +38,7 @@ const genericEmitHandler = (emitData) => {
 
 // Convery emits from the server directly into actions for the store
 socket.on('serverEmit', (emitData) => {
+  log(emitData);
   store.dispatch({
     type: 'CSRF',
     csrf: emitData.csrf,
