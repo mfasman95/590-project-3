d37945ecfa73928e0d409af3564bd655ad66ffd3
Matthew Fasman <mfasman95@gmail.com>
2017-12-07 16:26:52 -0500

    Add emit to database user functions

--- server/sockets/emit.js
+++ server/sockets/emit.js
@@ -1,7 +1,7 @@
 const { Message } = require('../classes');
-const { getUser } = require('./users');
-const xxh = require('xxhashjs');
+const { getUser, getUserByDBID } = require('./users');
 
+// #region Base Emit Functions
 // Build functions to emit to a single client
 const emit = type => message => (socket) => {
   const { event, data, timestamp } = message;
@@ -13,11 +13,13 @@ const emit = type => message => (socket) => {
     timestamp,
   });
 };
-const genericEmit = emit('generic');
-const reduxEmit = emit('redux');
+module.exports.genericEmit = emit('generic');
+module.exports.reduxEmit = emit('redux');
 
-const reduxErrorEmit = data => reduxEmit(new Message('ERROR_SNACK', data));
+module.exports.reduxErrorEmit = data => module.exports.reduxEmit(new Message('ERROR_SNACK', data));
+// #endregion Base Emit Functions
 
+// #region Emit To Room
 // Build functions to emit to a full room
 const emitToRoom = emitFunc => room => (message) => {
   for (let i = 0; i < room.currentOccupancy; i++) {
@@ -25,13 +27,15 @@ const emitToRoom = emitFunc => room => (message) => {
     if (user) emitFunc(message)(user.socket);
   }
 };
-const genericEmitToRoom = emitToRoom(genericEmit);
-const reduxEmitToRoom = emitToRoom(reduxEmit);
+module.exports.genericEmitToRoom = emitToRoom(module.exports.genericEmit);
+module.exports.reduxEmitToRoom = emitToRoom(module.exports.reduxEmit);
+// #endregion Emit To Room
 
-module.exports = Object.freeze({
-  genericEmit,
-  reduxEmit,
-  genericEmitToRoom,
-  reduxEmitToRoom,
-  reduxErrorEmit,
-});
+// #region Emit To Database User
+const emitToUser = emitFunc => userRowId => (message) => {
+  const user = getUserByDBID(userRowId);
+  if (user) emitFunc(message)(user.socket);
+};
+module.exports.genericEmitToUser = emitToUser(module.exports.genericEmit);
+module.exports.reduxEmitToUser = emitToUser(module.exports.reduxEmit);
+// #endregion Emit To Database User
