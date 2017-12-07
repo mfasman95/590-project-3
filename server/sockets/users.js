9930130d17105c34c394528686c3dfa4acf804c0
Matthew Fasman <mfasman95@gmail.com>
2017-12-07 16:18:19 -0500

    Add a getUserByDBID function for finding a connected user with the specified database ID to send socket messages to

--- server/sockets/users.js
+++ server/sockets/users.js
@@ -10,4 +10,26 @@ module.exports.addUser = (socket) => {
 
 module.exports.getUser = hash => users[hash];
 
+module.exports.getUserByDBID = (id) => {
+  const userKeys = Object.keys(users);
+
+  let retUser = null;
+
+  for (let i = 0; i < userKeys.length; i++) {
+    const user = users[userKeys[i]];
+
+    // If the user has disconnected while this function is running, skip past them
+    if (!user) continue;
+
+    // Check if this is the user you are looking for
+    if (user.userRowId === id) {
+      retUser = user;
+      // Shortcut when the user is found
+      break;
+    }
+  }
+
+  return retUser;
+};
+
 module.exports.deleteUser = hash => delete users[hash];
