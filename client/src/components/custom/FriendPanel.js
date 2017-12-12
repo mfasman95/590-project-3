8a8104aaad2ad9d0f5195fb9401e054abccde33c
Matthew Fasman <mfasman95@gmail.com>
2017-12-12 00:27:34 -0500

    Testing one layer too high

--- client/src/components/custom/FriendPanel.js
+++ client/src/components/custom/FriendPanel.js
@@ -5,6 +5,10 @@ import { emit } from './../../scripts/socket';
 class FriendPanel extends React.Component {
   render() {
     const { name, support, id } = this.props.friend;
+    let supportObj;
+    if (support) {
+      supportObj = support[Object.keys(support)[0]];
+    }
     return (
       <Panel>
         <Row>
@@ -12,14 +16,14 @@ class FriendPanel extends React.Component {
             <h4>{name}</h4>
           </Col>
           {
-            support ?
+            supportObj ?
               <Col xs={8} lg={6}>
                 <Row>
-                  <h4><u><b>{support.name}</b> - {support.race} {support.className} - Level {support.level}</u></h4>
+                  <h4><u><b>{supportObj.name}</b> - {supportObj.race} {supportObj.className} - Level {supportObj.level}</u></h4>
                 </Row>
                 <Row>
-                  <Col xs={6}><h4>Health: {support.hp}</h4></Col>
-                  <Col xs={6}><h4>Attack: {support.hit}</h4></Col>
+                  <Col xs={6}><h4>Health: {supportObj.hp}</h4></Col>
+                  <Col xs={6}><h4>Attack: {supportObj.hit}</h4></Col>
                 </Row>
               </Col>:
               <Col xs={8} lg={6}>
